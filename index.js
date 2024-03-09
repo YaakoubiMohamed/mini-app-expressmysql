bb506b27e3ab4fbf606904be1a3a5f449ef825c0
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-03-09 04:09:55 +0100

    up

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const compression = require('compression');
 
 const db = require('./models/index');
-  
-const  sequelize = db.sequelize;
-   
+
+const sequelize = db.sequelize;
+
 const app = express();
-   
-const PORT= process.env.APP_PORT;
-   
+app.use(compression());
+
+const PORT = process.env.APP_PORT;
+
 app.use(bodyparser.json());
 app.use(cors());
 const path = require('path');
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -28,23 +28,23 @@ const reclamationRoutes = require('./routes/reclamationRoutes')
 
 app.use('/api/auth', authRoutes)
 app.use('/api', userRoutes);
-app.use("/api",categorieRoutes)
-app.use("/api",produitRoutes)
-app.use("/api",commandeRoutes)
-app.use("/api",ligneCommandeRoutes)
-app.use("/api",messageRoutes)
-app.use("/api",reclamationRoutes)
+app.use("/api", categorieRoutes)
+app.use("/api", produitRoutes)
+app.use("/api", commandeRoutes)
+app.use("/api", ligneCommandeRoutes)
+app.use("/api", messageRoutes)
+app.use("/api", reclamationRoutes)
 
 
 sequelize.authenticate()
-.then(() => {
-  console.log('Connection has been established successfully.');
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
-});
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
-app.listen(4000, ()=>{
-    console.log("Server is running on port 4000")
+app.listen(4000, () => {
+  console.log("Server is running on port 4000")
 })
